146840b620a5500be05da2b611a24856cea26ad8
Sawyer Newman <sauuyer@gmail.com>
2024-04-13 10:28:38 -0400

    added runSampleFast. changed file store. fixed calculations.

--- make_csv_from_hdr_parse/gather_variables_from_hdr.py
+++ make_csv_from_hdr_parse/gather_variables_from_hdr.py
@@ -1,46 +1,63 @@
 import os
-import pandas
+import pandas as pd
 import re
+import chardet
 
-#dirx = "/Volumes/My Passport/IFCB_PLIMS/AR82_ifcb_data/AR82_ifcb206_all_data/D20240408T021656_IFCB206.roi"
-dir = "/Users/sawyer/Desktop/AR82-Pioneer20/ifcb-test-dir"
-
-#f = open(dir, 'r')
-
-
+dir = "/Volumes/My Passport/IFCB_PLIMS/AR82_ifcb_data/AR82_ifcb206_all_data/"
+#dir = "/Users/sawyer/Desktop/AR82-Pioneer20/ifcb-test-dir"
+#print(dir)
 #file_parameters = ["triggerCount", "roiCount", "humidity", "temperature", "runTime", "inhibitTime", "PMTAhighVoltage", "PMTBhighVoltage", "FileComment"]
+
 '''
 underway samples -> NumberSyringesToAutoRun: 10000
 discrete samples -> NumberSyringesToAutoRun: 3
 '''
 
+file_parameters = ["FileComment", "triggerCount", "roiCount", "runTime", "inhibitTime", "humidity", "temperature", "runTime",
+                       "PMTAhighVoltage", "PMTBhighVoltage", "humidity", "SyringeSampleVolume", "syringeSamplingSpeed", "temperature", "RunFastFactor",
+                   "sampleNumber", "runSampleFast"]
 
-def find_value_after_string(content):
-    #print(hdr_content)
-    file_parameters = ["triggerCount", "roiCount", "humidity", "temperature", "runTime", "inhibitTime",
-                       "PMTAhighVoltage", "PMTBhighVoltage", "FileComment"]
-    #for param in file_parameters:
-    pattern = re.compile(r'triggerCount:\s*(\d+)')
-    #print(pattern)
-    match = pattern.search(hdr_content)
-
-    if match:
-        value = match.group(1)
-        #return match.group(1)
-        print("value: ", value)
-    else: #return None
-        print("No match found.")
+all_fileparam_dicts = []
 
 
-for filename in os.listdir(dir):
-    if filename.endswith(".hdr"):
-        filepath = os.path.join(dir, filename)
-        with open(filepath, "r") as file:
-            hdr_content = file.read()
-            find_value_after_string(hdr_content)
+def gather_values(text_content, param_list):
+    file_dict = {"Filename": filename}
+    for p in param_list:
+        escaped_dynamic_string = re.escape(p)
+        pattern = re.compile(r'{}:\s*(.+)'.format(escaped_dynamic_string))
+        dynamic_match = pattern.search(hdr_content)
+
+        if dynamic_match:
+            value = dynamic_match.group(1)
+            #return dynamic_match.group(1)
+            #print(p, " value: ", value)
+            file_dict[p] = value
+        else: #return None
+            print(filename, "No match found.")
+
+    all_fileparam_dicts.append(file_dict)
 
 
-#value = find_value_after_string(search_string)
+for file in os.listdir(dir):
+    if file.endswith(".hdr"):
+        filepath = os.path.join(dir, file)
+        filename = os.path.splitext(file)[0]
+        #print(filepath)
+        with open(filepath, "r", encoding="iso-8859-1") as f:
+            #print(f)
+            hdr_content = f.read()
+            #print(hdr_content)
+            gather_values(hdr_content, file_parameters)
+
+df = pd.DataFrame(all_fileparam_dicts)
+
+
+
+print(df)
+
+df.to_csv("hdrFileSummaryFULL-TCadditions_2024-04-13.csv")
+
+
 
 
 
--- make_csv_from_hdr_parse/hdrSummaryProcessing.py
+++ make_csv_from_hdr_parse/hdrSummaryProcessing.py
@@ -1,31 +1,45 @@
 from datetime import datetime
 import pandas as pd
+import re
 
-file = "/Users/sawyer/Desktop/AR82-Pioneer20/ifcb-test-dir/ifcb-parsing-processing/make_csv_from_hdr_parse/hdrFileSummaryFULL.csv"
+file = "/Users/sawyer/Desktop/AR82-Pioneer20/ifcb-test-dir/ifcb-parsing-processing/make_csv_from_hdr_parse/hdrFileSummaryFULL-TCadditions_2024-04-13.csv"
 
+
+# read in processed file as a dataframe
 df = pd.read_csv(file)
+#drop rows where file names start with and underscore
+#df = df.drop(df[df['Filename'].str.contains('._')])
+df = df[~df['Filename'].str.startswith('._')]
+#df = df.drop(df[df['Filename'].str.contains('_')])
+
+print(df.head())
 
 df['Datetime'] = pd.to_datetime(df['Filename'].str[1:15], format='%Y%m%dT%H%M%S')
 
-print(5/df['runTime'])
-print(df['sampleTime']*df['sampleVol_per_second'])
-print(df['roiCount']*df['sampledVolume'])
 
-df['sampleTime'] = df['runTime'] - df['inhibitTime']
-df['sampleVol_per_second'] = 5/df['runTime']
-df['sampledVolume'] = df['sampleTime']*df['sampleVol_per_second']
-df['ROIsperSampledVol'] = df['roiCount']*df['sampledVolume']
-
-#calculated parameters from Taylor
+#lookTime
+df['lookTime'] = df['runTime'] - df['inhibitTime']
 
 #Volume Analyzed
-### looktime = runTime - inhibitTime
-### if runsamplefast = false then runsamplefast==1
-### flow rate = sampledVolume / runTime # (check on the time units for these)
+### if runsamplefast = false then runsamplefast==1 (correction from what is in original email)
+
+def get_runSampleFast_Int(runSampleFast):
+    if runSampleFast == False:
+        return 1
+    elif runSampleFast == True:
+        return 0
+
 ### what is flow rate - flowRate presents in ifcbAquire (20 mins per syringe)
 #### flow rate relevant details ::: 20 mins per syringe | 5 ml per syringe
-volumeAnalyzed = (runFastFactor * runSampleFast) * f
 
-print(df)
+df['runSampleFast_Int'] = df['runSampleFast'].apply(get_runSampleFast_Int)
 
-df.to_csv("hdrFileSummaryFullwithCalcs.csv")
+#### syringeSamplingSpeed (usually 20 for 20 mins), SyringeSampleVolume (usually 5 ml)
+df['flowRate_mins'] = df['SyringeSampleVolume'] / df['syringeSamplingSpeed']
+
+#### volume analyzed calc
+df['volumeAnalyzed'] = (df['RunFastFactor'] * df['runSampleFast_Int']) * df['flowRate_mins'] * (df['lookTime']/60)
+
+print(df['volumeAnalyzed'])
+
+df.to_csv("hdrFileSummaryFullwithCalcs_2024-04-13.csv", index = False)
